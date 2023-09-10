b04f9c24803300efc7c5271f645c55f6d6ebf3d4
rahat97 <iamrahat13@gmail.com>
2023-09-10 18:11:38 +0600

    madlib mini game

---
+++ madlib.py
@@ -0,0 +1,11 @@
+# put some strings together
+
+adj = input("Adjective about Computer programming: ")
+verb1 = input("Verb for Computer Programming: ")
+verb2 = input("Verb for cool Computer Programs: ")
+famous_person = input("Famous person for Computer Programming: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
